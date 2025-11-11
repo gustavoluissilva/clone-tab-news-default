4faa670e683ffe6da334a06703a46aafd9c2c33a
Gustavo Luis da Silva <desenvolvimento@criciuma.sc.gov.br>
2025-11-11 16:06:27 -0300

    adicionado página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Te amo!! 🥰 </h1>;
+  return <>
+  <h1>Site em Contrução!!</h1>
+  <img src="/images/logo_das_duas.jpg" alt="Site em construção" width={300} />
+  <p>by: Gustavo Luis da Silva </p>
+  </>;
 }
 
 export default Home;
