d16bfefc6bd8f2e4b0d8801a8d334d43a1266ace
Gustavo Luis da Silva <desenvolvimento@criciuma.sc.gov.br>
2026-01-09 16:49:11 -0300

    add the solution to solve the problem propose to me

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,21 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const versionDb = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+  const connectionsMax = (await database.query("SHOW max_connections;")).rows[0]
+    .max_connections;
+  const connectionsNow = (
+    await database.query("SELECT COUNT(*) FROM pg_stat_activity;")
+  ).rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version_db: versionDb,
+    connections_max: parseInt(connectionsMax),
+    connections_now: parseInt(connectionsNow),
+  });
 }
 
 export default status;
--- tests/integration tests/get.test.js
+++ tests/integration tests/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdadedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdadedAt);
+
+  expect(responseBody.version_db).toBe("16.0");
+  expect(responseBody.connections_max).toBe(100);
+  expect(responseBody.connections_now).toBeGreaterThanOrEqual(1);
 });
