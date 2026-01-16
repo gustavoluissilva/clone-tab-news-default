14974da1fb8ca1e0a3fb069ead94ece8ee4b072a
Gustavo Luis da Silva <desenvolvimento@criciuma.sc.gov.br>
2026-01-16 13:32:40 -0300

    image

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,6 @@ function Home() {
       <div className="background">
         <h1>Site em Contrução!!</h1>
 
-        <img
-          src="/images/caco.jpg"
-          alt="Under Construction"
-          className="construction-image"
-        />
-
         <div className="🤚">
           <div className="👉"></div>
           <div className="👉"></div>
