1b54525efa6ed4117c553908d01d290760a5ecac
Gustavo Luis da Silva <desenvolvimento@criciuma.sc.gov.br>
2026-01-16 12:18:57 -0300

    test new feature

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 function Home() {
-  return <>
-  <h1>Site em Contrução!!</h1>
-  <img src="/images/logo_das_duas.jpg" alt="Site em construção" width={300} />
-  <p>by: Gustavo Luis da Silva </p>
-  </>;
+  return (
+    <>
+      <link rel="stylesheet" href="./style.css" />
+      <h1>Site em Contrução!!</h1>
+      <div class="🤚">
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="🌴"></div>
+        <div class="👍"></div>
+      </div>
+      <img
+        src="/images/logo_das_duas.jpg"
+        alt="Site em construção"
+        width={300}
+      />
+      <p>by: Gustavo Luis da Silva </p>
+    </>
+  );
 }
 
 export default Home;
